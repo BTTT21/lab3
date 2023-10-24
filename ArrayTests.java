f5805d1fc0815a5ec371ae41a47fb5bc2e2cc5da
BTTT21 <bryantzhu664@gmail.com>
2023-10-24 11:22:33 -0700

    update

--- ArrayTests.java
+++ ArrayTests.java
@@ -12,7 +12,13 @@ public class ArrayTests {
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 1,2,3,4,5};
+    assertArrayEquals(new int[]{ 5,4,3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAvg(){
+    double[] arr = {0.1,0.1,0.1,0.1,0.1,0.1};
+    assertEquals(0.1, ArrayExamples.averageWithoutLowest(arr), 0.0001);
   }
 }
